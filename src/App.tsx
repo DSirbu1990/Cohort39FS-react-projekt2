45b28a892e6d81f3b3e3d2c107cd1e5cc90f1bab
DSirbu1990 <doina.strilciuc@gmail.com>
2024-07-06 15:08:00 +0200

    lesson07 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import './App.css';
+// Lessons
+import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
+import Lesson_07 from "./lessons/Lesson_07/Lesson_07";
+
+// Homeworks
+import Homework_06 from "./homeworks/Hommework_06/Hommework_06";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      React TypeScript App
+      {/* <Lesson_06 /> */}
+      {/* <Homework_06 /> */}
+      <Lesson_07 />
     </div>
   );
 }
---
+++ src/lessons/Lesson_06/Lesson_06.tsx
@@ -0,0 +1,13 @@
+import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Lesson_06 />
+    </div>
+  );
+}
+
+export default App;

53c3fd6e317f790ee4bd4562a7a719778c7e7f7d
DSirbu1990 <doina.strilciuc@gmail.com>
2024-07-15 19:18:39 +0200

    generally commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
+import GlobalStyles from "styles/GlobalStyles";
+
 // Lessons
 import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
 import Lesson_07 from "./lessons/Lesson_07/Lesson_07";
+import Lesson_08 from "lessons/Lesson_08/Lesson_08";
+import Lesson_09 from "lessons/Lesson_09/Lesson_09";
 
 // Homeworks
-import Homework_06 from "./homeworks/Hommework_06/Hommework_06";
+import Homework_06 from "./homeworks/Homework_06/Homework_06";
+import Homework_07 from "homeworks/Homework_07/Homewotk_07";
+import Homework_08 from "homeworks/Homework_08/Homework_08";
+import Homework_09 from "homeworks/Homework_09/Homework_09";
 
-import "./App.css";
+// Consultations
+//import Consultation_03 from "consultations/Consultation_03/Consultation_03";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <GlobalStyles />
+      {/* <Consultation_03 /> */}
       {/* <Lesson_06 /> */}
+      {/* <Lesson_07 /> */}
+      {/* <Lesson_08 /> */}
+      {/* <Lesson_09 /> */}
       {/* <Homework_06 /> */}
-      <Lesson_07 />
-    </div>
+      {/* <Homework_07 /> */}
+      {/* <Homework_08 /> */}
+      <Homework_09 />
+    </>
   );
 }
 
